f35025362ab6f283b39de96987ef35ae9fcfec21
Misskhat <misskhat.hossen@gmail.com>
2025-10-10 20:13:34 +0600

    barchart added

--- src/Pages/AppDetails/AppDetails.jsx
+++ src/Pages/AppDetails/AppDetails.jsx
@@ -1,3 +1,4 @@
+import { Bar, BarChart, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import download from '../../assets/icon-downloads.png'
 import rating from '../../assets/icon-ratings.png'
 import review from '../../assets/icon-review.png'
@@ -12,18 +13,18 @@ const AppDetails = () => {
     // console.log(data);
     const clickApp = data.find(app => app.id === idConvert);
     // console.log(clickApp);
-    const {image, title, companyName, description, size, reviews, ratingAvg, downloads} = clickApp
+    const {image, title, companyName, description, size, reviews, ratingAvg, downloads, ratings} = clickApp
     return (
-        <div className="w-[1280px] mx-auto my-20">
-            <div className="flex gap-10">
-                <div className="w-[350px] h-[350px] rounded">
-                    <img className='w-[350px] h-[350px] object-cover' src={image} alt="" />
+        <div className="md:w-[1280px] mx-auto md:my-20 my-10">
+            <div className="md:flex gap-10">
+                <div className="md:w-[350px] md:h-[300px] rounded">
+                    <img className='md:w-[350px] md:h-[350px] object-cover' src={image} alt="" />
                 </div>
-                <div className="flex-1">
-                    <h3 className="text-4xl font-bold"> {title} </h3>
+                <div className="md:flex-1">
+                    <h3 className="md:text-4xl text-2xl font-bold"> {title} </h3>
                     <p className="font-semibold text-[#627382]">Developed by <span className="text-[#9F62F2]">{companyName} </span></p>
                     <div className="divider"></div>
-                    <div className="flex gap-10 items-center mt-8">
+                    <div className="flex max-sm:flex-col gap-10 items-center mt-8">
                         <div>
                             <div>
                                 <img className='w-[40px] h-[40px]' src={download} alt="" />
@@ -50,6 +51,14 @@ const AppDetails = () => {
                 </div>
             </div>
             <div className='divider'></div>
+            <BarChart width={700} height={250} data={ratings} >
+                <YAxis></YAxis>
+                <XAxis dataKey={'name'} ></XAxis>
+                <Tooltip></Tooltip>
+                {/* <Legend></Legend> */}
+                <Bar dataKey={'count'} fill='#FF8811' layout='horizontal'></Bar>
+            </BarChart>
+            <div className='divider '></div>
             <h3 className='text-2xl font-bold mt-10 mb-6'>Description</h3>
             <div className='text-[#627382]'>
                 <p className='text-justify'> {description} </p>
